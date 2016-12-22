c120da5a2bd3aa8d5a8b00036d1358afe457cde2
Ricardo Polo <ricardo.polo@tulpep.com>
2016-12-22 13:19:46 -0500

    Options class for parameters

---
+++ Tulpep.InternetSimulator/Tulpep.InternetSimulator/Options.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Tulpep.InternetSimulator
+{
+    public class Options
+    {
+        [Option('r', "read", Required = true,
+          HelpText = "Input file to be processed.")]
+        public string InputFile { get; set; }
+
+        [Option('v', "verbose", DefaultValue = true,
+          HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+
+}
--- Tulpep.InternetSimulator/Tulpep.InternetSimulator/Program.cs
+++ Tulpep.InternetSimulator/Tulpep.InternetSimulator/Program.cs
@@ -10,6 +10,11 @@ namespace Tulpep.InternetSimulator
     {
         static void Main(string[] args)
         {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                if (options.Verbose) Console.WriteLine("Filename: {0}", options.InputFile);
+            }
         }
     }
 }
