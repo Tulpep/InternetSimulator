fd9b385202a5ef6c288ebb949fdca8a488b46db4
Ricardo Polo <ricardo.polo@tulpep.com>
2016-12-23 14:15:14 -0500

    Simpler code

--- Tulpep.InternetSimulator/Tulpep.InternetSimulator/FilesController.cs
+++ Tulpep.InternetSimulator/Tulpep.InternetSimulator/FilesController.cs
@@ -1,31 +1,22 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Controllers;
 
 namespace OwinSelfhostSample
 {
     public class FilesController : ApiController
     {
-        public async Task<HttpResponseMessage> Get()
+        public HttpResponseMessage Get()
         {
             var requestedUri = Request.RequestUri.AbsoluteUri;
             var localFilePath = @"C:\hello.txt";
 
-            if(File.Exists(localFilePath))
-            {
-                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
-                response.Content = new StreamContent(new FileStream(localFilePath, FileMode.Open, FileAccess.Read));
-                return response;
-            }
-            else
-            {
-                return Request.CreateResponse(HttpStatusCode.OK, requestedUri);
-            }
+            if(!File.Exists(localFilePath)) return Request.CreateResponse(HttpStatusCode.NotFound);
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(new FileStream(localFilePath, FileMode.Open, FileAccess.Read));
+            return response;
 
         }
     }
