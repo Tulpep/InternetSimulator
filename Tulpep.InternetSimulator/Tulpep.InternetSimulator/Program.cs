594a97a8055e2efe40ddcd9d82cd1263e9a15454
Ricardo Polo <ricardo.polo@tulpep.com>
2017-01-05 14:54:30 -0500

    DNS simulator

--- Tulpep.InternetSimulator/Tulpep.InternetSimulator/Program.cs
+++ Tulpep.InternetSimulator/Tulpep.InternetSimulator/Program.cs
@@ -1,39 +1,107 @@
-﻿using Microsoft.Owin.Hosting;
+﻿using ARSoft.Tools.Net;
+using ARSoft.Tools.Net.Dns;
+using Microsoft.Owin.Hosting;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Tulpep.InternetSimulator
 {
     class Program
     {
+        private static Options _options = new Options();
         static void Main(string[] args)
         {
-            var options = new Options();
-            if (CommandLine.Parser.Default.ParseArguments(args, options))
-            {
-                if (options.Verbose) Console.WriteLine("Filename: {0}", options.InputFile);
-            }
+            CommandLine.Parser.Default.ParseArguments(args, _options);
 
-
-            string hostFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts");
-            string hostFileBackup = hostFile + "_Backup_Internet_Simulator";
-
-            List<string> hostModifications = new List<string>();
-            hostModifications.Add("microsoft.com");
-            hostModifications.Add("go.microsoft.com");
-            hostModifications.Add("google.com");
-
-            bool backupSuccess = BackupHostFile(options, hostFile, hostFileBackup);
-            if (!backupSuccess)
+            bool isAdmin = true;
+            if (!isAdmin)
             {
                 Console.WriteLine("Cannot modify HOSTS file. Run this program as Admininistrator");
                 return;
             }
 
-            ModifyHostFile(options, hostFile, hostModifications);
-            StartWebServer(options);
-            RestoreHostFile(options, hostFile, hostFileBackup);
+            StartDnsServer();
+        }
+
+        static void StartDnsServer()
+        {
+            using (DnsServer server = new DnsServer(10, 10))
+            {
+                server.ClientConnected += OnClientConnected;
+                server.QueryReceived += OnDnsQueryReceived;
+                server.Start();
+
+                Console.WriteLine("Press any key to stop server");
+                Console.ReadLine();
+            }
+        }
+
+        static async Task OnClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            if (!IPAddress.IsLoopback(e.RemoteEndpoint.Address))
+            {
+                e.RefuseConnect = true;
+                if (_options.Verbose) Console.WriteLine("Denied access to DNS Client " + e.RemoteEndpoint.Address);
+            }
+        }
+
+        static async Task OnDnsQueryReceived(object sender, QueryReceivedEventArgs e)
+        {
+
+            string server = "8.8.8.8";
+            DnsMessage message = e.Query as DnsMessage;
+
+            if (message == null)
+                return;
+
+            DnsMessage response = message.CreateResponseInstance();
+
+            if ((message.Questions.Count == 1))
+            {
+                //If domain match return localhost
+                DnsQuestion question = message.Questions[0];
+                if(question.RecordType ==  RecordType.A && question.Name.Equals(DomainName.Parse("microsoft.com")))
+                {
+                    response.ReturnCode = ReturnCode.NoError;
+                    response.AnswerRecords.Add(new ARecord(question.Name, 10, IPAddress.Parse("127.0.0.1")));
+                }
+                else
+                {
+                    // send query to upstream server
+                    DnsClient dnsClient = new DnsClient(IPAddress.Parse(server), 5000);
+                    DnsMessage upstreamResponse = await dnsClient.ResolveAsync(question.Name, question.RecordType, question.RecordClass);
+                   
+                    // if got an answer, copy it to the message sent to the client
+                    if (upstreamResponse != null)
+                    {
+                        foreach (DnsRecordBase record in (upstreamResponse.AnswerRecords))
+                        {
+                            response.AnswerRecords.Add(record);
+                        }
+                        foreach (DnsRecordBase record in (upstreamResponse.AdditionalRecords))
+                        {
+                            response.AdditionalRecords.Add(record);
+                        }
+
+
+                        response.ReturnCode = ReturnCode.NoError;
+                    }
+                }
+
+                // set the response
+                e.Response = response;
+
+                if (_options.Verbose)
+                {
+                    if(response.AnswerRecords.Count != 0 ) Console.WriteLine("DNS Response: {0}", response.AnswerRecords.FirstOrDefault());
+                    else Console.WriteLine("Cannot find {0} records for {1}", question.RecordType.ToString().ToUpperInvariant(), question.Name);
+                }
+
+            }
         }
 
         static void StartWebServer(Options options)
@@ -43,8 +111,8 @@ namespace Tulpep.InternetSimulator
             try
             {
                 WebApp.Start<WebServerStartup>(baseUri);
-                Console.WriteLine("Server running at {0} - press Enter to quit. ", baseUri);
-                Console.ReadLine();
+                Console.WriteLine("Server running at {0} - press Enter to quit. ");
+               // Console.ReadLine();
             }
             catch (Exception ex)
             {
@@ -53,40 +121,6 @@ namespace Tulpep.InternetSimulator
 
         }
 
-        static bool BackupHostFile(Options options, string originalPath, string backupPath)
-        {
-            if (options.Verbose) Console.WriteLine("Creating backup of HOSTS file in " + originalPath);
-            try
-            {
-                File.Copy(originalPath, backupPath, true);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        static void RestoreHostFile(Options options, string originalPath, string backupPath)
-        {
-            if (options.Verbose) Console.WriteLine("Restoring backup of HOSTS file to " + originalPath);
-            File.Copy(backupPath, originalPath, true);
-            if (options.Verbose) Console.WriteLine("Deleting backup file in " + backupPath);
-            File.Delete(backupPath);
-        }
-
-        static void ModifyHostFile(Options options, string hostFilePath, IEnumerable<string> hostModifications)
-        {
-            using (StreamWriter writer = File.AppendText(hostFilePath))
-            {
-                foreach (string entry in hostModifications)
-                {
-                    if (options.Verbose) Console.WriteLine(String.Format("Adding domain {0} to HOSTS file", entry));
-                    writer.WriteLine(String.Format("{0}\t{1}", "127.0.0.1", entry));
-                }
-
-            }
-        }
 
     }
 }
